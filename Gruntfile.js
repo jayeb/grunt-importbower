7dc02763b26a1766a6aa21cd8494f6a441b2f0d2
Jason Eberle <jason@imgix.com>
2015-05-14 02:11:04 -0700

    Add test grunt task and supporting files

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,10 +18,22 @@ module.exports = function(grunt) {
             config: '.jscsrc',
             reporter: require('reporter-plus/jscs').path
           }
+      },
+
+    importbower: {
+        test: {
+            options: {
+                css_dest: '.tmp/css',
+                js_dest: '.tmp/js'
+              },
+            files: {
+                '.tmp/test.html': 'test/test.html'
+              }
+          }
       }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs');
-  // grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
 };
